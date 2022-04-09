19264a2353e9917fa1370a66a2455a0b84ac7892
szerpadiaz <szerpadiaz@gmail.com>
2022-04-09 19:50:57 +0200

    (Exercise 3): refactor button class

--- STM_SandBox/exercise_3_blink_led_while_button_is_pressed/Core/Src/Application/button.cpp
+++ STM_SandBox/exercise_3_blink_led_while_button_is_pressed/Core/Src/Application/button.cpp
@@ -9,7 +9,7 @@
 #include <Machine/gpio.h>
 #include <Machine/io_mapping.h>
 
-volatile uint32_t last_changed_time = 0;
+volatile uint32_t last_irq_time = 0;
 volatile bool button_pressed = false;
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
@@ -17,12 +17,12 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     if(GPIO_Pin == USER_BUTTON_PIN)
     {
     	uint32_t current_time = HAL_GetTick();
-    	if ((last_changed_time - current_time) > 15)
+    	if ((last_irq_time - current_time) > 15)
     	{
     		// When the button is held-down, the signal is high (GPIO_PIN_SET, connected to power). Otherwise, the signal is low (connected to ground).
     		button_pressed = (HAL_GPIO_ReadPin(USER_BUTTON_PORT, USER_BUTTON_PIN) == GPIO_PIN_SET);
     	}
-    	last_changed_time = current_time;
+    	last_irq_time = current_time;
     }
 }
 
